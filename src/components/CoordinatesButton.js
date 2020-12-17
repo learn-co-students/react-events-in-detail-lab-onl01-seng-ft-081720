cbabfa8c7f93a25e4a4d2d57aa286a42bf7a589a
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-17 16:15:34 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react'
+
+
+
+export default class CoordinatesButton extends React.Component{
+    whereIsThis = (event) => {
+   this.props.onReceiveCoordinates([event.clientY, event.clientX])
+
+    }
+    render(){
+        return(
+            <button onClick={this.whereIsThis}></button>
+        )
+    }
+}
+
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
-// Code DelayedButton Component Here
+// Code CoordinatesButton Component Here
+import React from 'react'
+
+
+
+export default class DelayedButton extends React.Component{
+
+    slowed = (e) => {
+        e.persist();
+        setTimeout(() => {
+            this.props.onDelayedClick(e)
+        })
+    }
+   
+    render(){
+        return(
+           <button onClick={this.slowed}></button>
+        )
+    }
+}
